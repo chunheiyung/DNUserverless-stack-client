b96e95cdae24aa7140d384f3121d25e97febc3f0
Chun Yung <chunheiyung@gmail.com>
2021-03-23 15:32:04 -0500

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "cy-notes-app-upload",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://ir6sgg94tb.execute-api.us-east-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_y5gwZmcFw",
+    APP_CLIENT_ID: "7t54sed9dp97nq7sc06o78975v",
+    IDENTITY_POOL_ID: "us-east-2:c327b68b-6245-4c08-a0d8-823a8eb9dfb5",
+  },
+};
+
+export default config;
